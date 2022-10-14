6c022ac3764cd5f6a96d329d7df19d2a148d704c
Ariel Wang <arielxiewang@gmail.com>
2022-10-14 11:37:38 +1100

    Make Changes According to Commit Review

--- app/src/main/java/com/arielwang/workoutlogger/features/track/ui/screen/TrackScreen.kt
+++ app/src/main/java/com/arielwang/workoutlogger/features/track/ui/screen/TrackScreen.kt
@@ -1,8 +1,25 @@
 package com.arielwang.workoutlogger.features.track.ui.screen
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.arielwang.workoutlogger.R
 import com.arielwang.workoutlogger.features.component.PrimaryButton
 
@@ -15,5 +32,31 @@ fun TrackScreen(
         modifier = Modifier,
         buttonText = stringResource(id = R.string.TrackScreen_buttonContent),
         onAction = { onAction(TrackView.Action.GoToNextPage) }
+    )
+
+    NumberTextField()
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun NumberTextField(
+
+) {
+    var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it},
+        textStyle = TextStyle.Default.copy(fontSize = 16.sp),
+        colors = TextFieldDefaults
+            .outlinedTextFieldColors(backgroundColor = MaterialTheme.colors.background),
+        modifier = Modifier
+            .height(52.dp)
+            .width(80.dp),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        keyboardActions = KeyboardActions(
+            onDone = { focusManager.clearFocus() }
+        )
     )
 }
